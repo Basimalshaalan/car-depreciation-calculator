dc5134b8958c2e15a2f8223339d18b1f1a80d472
Besmelh Alshaalan <besmelh.s@gmail.com>
2023-01-03 21:40:13 +0300

    added demo button code

---
+++ script.js
@@ -0,0 +1,10 @@
+// clicking submit
+function calculate() {
+  // get the value in the age input
+  let form_age = document.getElementById('age').value;
+  // add 2 to the value
+  // since javascript reads in the value as string, we must first convert it into an integer using parseInt
+  let new_age = parseInt(form_age) + 2;
+  document.getElementById('result').getElementsByTagName('span')[0].innerHTML =
+    new_age;
+}
