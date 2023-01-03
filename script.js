9a2d034578f0124039d127125970af7b490dd402
Besmelh Alshaalan <besmelh.s@gmail.com>
2023-01-03 22:15:13 +0300

    submit button works, and cleaning up the form styling

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function calculate() {
   document.getElementById('result').getElementsByTagName('span')[0].innerHTML =
     new_age;
 }
+
+function demo() {
+  // get the value in the age input
+  let form_age = document.getElementById('age').value;
+  // add 2 to the value
+  // since javascript reads in the value as string, we must first convert it into an integer using parseInt
+  let new_age = parseInt(form_age) + 10;
+  document.getElementById('result').getElementsByTagName('span')[0].innerHTML =
+    new_age;
+}
+
+// document.getElementsByTagName('form')[0].on('submit', demo());
